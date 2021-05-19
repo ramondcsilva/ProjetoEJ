71aea53aaa32570b4460b1c38e1177eb1e9bb1fa
Ramon Silva <ramondecerqueirasilva@gmail.com>
2021-05-19 18:13:06 -0300

    Update Navbar paddig

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -12,27 +12,28 @@
   </head>
   <body>
     <div class="stylish-form">
-      <nav class="nav-header">
-        <ul class="menu">
-          <img class="img-nav" src="https://ecompjr.com.br/images/logoNome.png" alt="">
-          <li class="logo"></li>
-          @auth
-            <li class="item"><a href="{{ url('home')}}">Home</a></li>
-            <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
-            <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
-            <li class="item"><a href="{{ url('enterprise')}}">Cadastrar Empresas Junior</a></li>
-            <li class="item"><a href="{{ url('federation')}}">Cadastrar Federação</a></li>
+      <div class="nav-header">
+        <nav>
+          <ul class="menu" style="padding-left:0rem;" >
+            <img class="img-nav" src="https://ecompjr.com.br/images/logoNome.png" alt="">
+            <li class="logo"></li>
+            @auth
+              <li class="item"><a href="{{ url('home')}}">Home</a></li>
+              <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
+              <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
+              <li class="item"><a href="{{ url('enterprise')}}">Cadastrar Empresas Junior</a></li>
+              <li class="item"><a href="{{ url('federation')}}">Cadastrar Federação</a></li>
 
-            <li class="item button secondary"><a href="{{ route('logout')}}">Logout</a></li>
-          @else
-            <li class="item"><a href="{{ url('home')}}">Home</a></li>
-            <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
-            <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
-            <li class="item button"><a href="{{ url('login')}}">Log In</a></li>
-          @endauth 
-        </ul>
-      </nav>
-      
+              <li class="item button secondary"><a href="{{ route('logout')}}">Logout</a></li>
+            @else
+              <li class="item"><a href="{{ url('home')}}">Home</a></li>
+              <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
+              <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
+              <li class="item button"><a href="{{ url('login')}}">Log In</a></li>
+            @endauth 
+          </ul>
+        </nav>
+      </div>
       <div class="recent-grid">
         <div class="project">
           <div class="card">
--- resources/views/home/listenterprises.blade.php
+++ resources/views/home/listenterprises.blade.php
@@ -10,91 +10,90 @@
   </head>
   <body>
     <div class="stylish-form">
-      <nav class="nav-header">
-        <ul class="menu">
-          <img class="img-nav" src="https://ecompjr.com.br/images/logoNome.png" alt="">
-          <li class="logo"></li>
-          @auth
-            <li class="item"><a href="{{ url('home')}}">Home</a></li>
-            <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
-            <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
-            <li class="item"><a href="{{ url('enterprise')}}">Cadastrar Empresas Junior</a></li>
-            <li class="item"><a href="{{ url('federation')}}">Cadastrar Federação</a></li>
+      <div class="nav-header">
+        <nav>
+          <ul class="menu">
+            <img class="img-nav" src="https://ecompjr.com.br/images/logoNome.png" alt="">
+            <li class="logo"></li>
+            @auth
+              <li class="item"><a href="{{ url('home')}}">Home</a></li>
+              <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
+              <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
+              <li class="item"><a href="{{ url('enterprise')}}">Cadastrar Empresas Junior</a></li>
+              <li class="item"><a href="{{ url('federation')}}">Cadastrar Federação</a></li>
 
-            <li class="item button secondary"><a href="{{ route('logout')}}">Logout</a></li>
-          @else
-            <li class="item"><a href="{{ url('home')}}">Home</a></li>
-            <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
-            <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
-            <li class="item button"><a href="{{ url('login')}}">Log In</a></li>
-          @endauth
-        </ul>
-      </nav>
-      
+              <li class="item button secondary"><a href="{{ route('logout')}}">Logout</a></li>
+            @else
+              <li class="item"><a href="{{ url('home')}}">Home</a></li>
+              <li class="item"><a href="{{ url('services')}}">Serviços</a></li>
+              <li class="item"><a href="{{ url('enterprise/list')}}">Empresas Junior</a></li>
+              <li class="item button" style="line-height: 1.5;"><a href="{{ url('login')}}">Log In</a></li>
+            @endauth
+          </ul>
+        </nav>
+      </div>
       <div class="recent-grid">
         <div class="project">
-          <div class="card">
-            <div class="container px-4 py-5" id="featured-3">          
-              <div class="card-header">
-                <h3>Empresas Junior</h3>
-                <div class="input-group">
-                  <form class="input-group-addon" action="{{ route('/enterprise/list') }}" method="post">  
-                  @csrf 
-                    <select class="form-control" name="selected">
-                      <option class="form-option" value="0">Todos</option>
-                      @foreach($federation as $f)
-                        <option class="form-option" value="{{$f->id}}">{{$f->name}}</option>
-                      @endforeach
-                    </select>
-                    <button type="submit">Pesquisar</button>
-                  </form>
-                </div>
-                <div class="input-group">
-                  <form class="input-group-addon" action="{{ route('/enterprise/list/unique') }}" method="post">  
-                  @csrf 
-                    <input class="form-control" type="text" name="search">
-                    <button type="submit">Pesquisar</button>
-                  </form>
-                </div>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive">
-                  <table width="100%">
-                    <thead>
-                      <tr>
-                        <td>Nome</td>
-                        <td>Federação</td>
-                        <td>Estado</td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($enterprises as $e)
-                      @if($search == $e->name)
-                        <tr>
-                          <td>{{$e->name}}</td>
-                          <td>{{$e->federation->name}}</td>
-                          <td>{{$e->federation->state->name}}</td>
-                        </tr>
-                      @elseif($search == null && $select == $e->federation_id)
-                        <tr>
-                          <td>{{$e->name}}</td>
-                          <td>{{$e->federation->name}}</td>
-                          <td>{{$e->federation->state->name}}</td>
-                        </tr>
-                      @elseif($search == null && $select == 0)  
-                        <tr>
-                          <td>{{$e->name}}</td>
-                          <td>{{$e->federation->name}}</td>
-                          <td>{{$e->federation->state->name}}</td>
-                        </tr>
-                      @endif
+          <div class="card">        
+            <div class="card-header">
+              <h3>Empresas Junior</h3>
+              <div class="input-group">
+                <form class="input-group-addon" action="{{ route('/enterprise/list') }}" method="post">  
+                @csrf 
+                  <select class="form-control" name="selected">
+                    <option class="form-option" value="0">Todos</option>
+                    @foreach($federation as $f)
+                      <option class="form-option" value="{{$f->id}}">{{$f->name}}</option>
                     @endforeach
-                    </tbody>
-                  </table>
-                </div>  
+                  </select>
+                  <button type="submit">Pesquisar</button>
+                </form>
               </div>
-            </div>  
-          </div>
+              <div class="input-group">
+                <form class="input-group-addon" action="{{ route('/enterprise/list/unique') }}" method="post">  
+                @csrf 
+                  <input class="form-control" type="text" name="search">
+                  <button type="submit">Pesquisar</button>
+                </form>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table width="100%">
+                  <thead>
+                    <tr>
+                      <td>Nome</td>
+                      <td>Federação</td>
+                      <td>Estado</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  @foreach ($enterprises as $e)
+                    @if($search == $e->name)
+                      <tr>
+                        <td>{{$e->name}}</td>
+                        <td>{{$e->federation->name}}</td>
+                        <td>{{$e->federation->state->name}}</td>
+                      </tr>
+                    @elseif($search == null && $select == $e->federation_id)
+                      <tr>
+                        <td>{{$e->name}}</td>
+                        <td>{{$e->federation->name}}</td>
+                        <td>{{$e->federation->state->name}}</td>
+                      </tr>
+                    @elseif($search == null && $select == 0)  
+                      <tr>
+                        <td>{{$e->name}}</td>
+                        <td>{{$e->federation->name}}</td>
+                        <td>{{$e->federation->state->name}}</td>
+                      </tr>
+                    @endif
+                  @endforeach
+                  </tbody>
+                </table>
+              </div>  
+            </div>
+          </div>  
         </div>
       </div> 
     </div>
